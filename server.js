ba0c36fd19aaced6f64fce52888e710f47f4718c
progToMax <m@m.com>
2019-12-22 23:16:43 -0600

    finishing touches putting post and get and use api

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ projectData = {};
 const express = require("express");
 // Start up an instance of app
 const app = express();
-
+const http = require('http');
 /* Middleware*/
 const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
@@ -17,14 +19,118 @@ const cors = require('cors');
 app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
-
+const api = '4f1dc020feb035971c309f2a30e0f383';
+const baseurl = 'http://api.openweathermap.org/data/2.5/weather?q=#location#&APPID=';
 
 const router = express.Router();
-router.get("/weather/:city", function(req, res) {
-    const citywet = [
-        { city: 'Houston', wet: 73 },
-        { city: 'Philadelphia', wet: 53 },
-        { city: 'Dallas', wet: 70 }
+
+function getdata(url, res) {
+    var Request = require("request");
+
+    Request.get({
+        "headers": {
+            "content-type": "application/json"
+        },
+        "url": url
+    }, (error, response, body) => {
+        var mess;
+        console.log('getting data ', body);
+        if (error) {
+            mess = JSON.strzingify({
+                success: false,
+                error: 'error'
+            });
+            console.log(error);
+            res.send(mess);
+        }
+        try {
+            body = JSON.parse(body);
+            const datasendback = {
+                temp: body.main.temp,
+                feel: 'great post',
+                date: body.sys.sunrise
+            }
+            mess = JSON.stringify(datasendback);
+            res.send(mess);
+        } catch (err) {
+            mess = JSON.stringify({
+                success: false,
+                error: 'error'
+            });
+            res.send(mess);
+        }
+
+    });
+}
+router.post('/', async (req, res) => {
+    console.log('res body ', req.body);
+    if (!req.body.zip) {
+        console.log("no zip defind")
+        res.send('system error');
+        return;
+    }
+    const url = baseurl.replace('#location#', req.body.zip) + api;
+
+    console.log('url ', url);
+    getdata(url, res);
+
+});
+router.get('/all/:loc/:content/:apikey', async function (req, res) {
+    let data;
+    console.log('params ', req.params);
+
+    var Request = require("request");
+
+    Request.get({
+        "headers": {
+            "content-type": "application/json"
+        },
+        "url": baseurl.replace('#location#', req.params.loc) + req.params.apikey
+    }, (error, response, body) => {
+        if (error) {
+
+            res.send({
+                success: false,
+                error: 'error'
+            });
+            return console.log(error);
+        }
+        console.log('obdy is ', body);
+        try {
+            body = JSON.parse(body);
+            const datasendback = {
+                temp: body.main.temp,
+                feel: req.params.content,
+                date: body.sys.sunrise
+            }
+            res.send(JSON.stringify(datasendback));
+            return;
+        } catch (err) {
+            res.send({
+                success: false,
+                error: 'error'
+            });
+            return;
+        }
+
+    });
+
+
+    // console.log('data is '  , data);
+});
+router.get("/weather/:city", function (req, res) {
+    const citywet = [{
+            city: 'Houston',
+            wet: 73
+        },
+        {
+            city: 'Philadelphia',
+            wet: 53
+        },
+        {
+            city: 'Dallas',
+            wet: 70
+        }
 
     ];
 
@@ -35,14 +141,24 @@ router.get("/weather/:city", function(req, res) {
     if (wet.length !== 0) {
         temp = wet[0].wet;
     }
-    res.send({ wet: temp });
+    res.send({
+        wet: temp
+    });
 });
-router.get("/zipcode/:zip", function(req, res) {
+router.get("/zipcode/:zip", function (req, res) {
     console.log('in zipcode')
-    const zipwet = [
-        { zip: 63101, wet: 53 },
-        { zip: 90210, wet: 63 },
-        { zip: 01010, wet: 48 }
+    const zipwet = [{
+            zip: 63101,
+            wet: 53
+        },
+        {
+            zip: 90210,
+            wet: 63
+        },
+        {
+            zip: 01010,
+            wet: 48
+        }
     ];
     console.log('params is ', req.params);
     const zip = req.params.zip;
@@ -56,7 +172,9 @@ router.get("/zipcode/:zip", function(req, res) {
     } else {
         temp = wet[0].wet;
     }
-    res.send({ wet: temp });
+    res.send({
+        wet: temp
+    });
 });
 app.use(router);
 // Setup Server
--- website/app.js
+++ website/app.js
@@ -3,12 +3,65 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+const apikey = '4f1dc020feb035971c309f2a30e0f383';
+const retrieveData = async () => {
+    const request = await fetch(`/all/${document.querySelector('#zip').value}/${document.all('feelings').value}/${apikey}`);
+    try {
+        // Transform into JSON
+        const allData = await request.json();
+        console.log(allData)
+        return allData;
+    } catch (error) {
+        console.log("error", error);
+        alert('System Error');
+    }
 
-(function() {
-    document.querySelector("#generate").addEventListener("click", function(e) {
-        fetch(`zipcode/${document.querySelector('#zip').value}`).then(x => x.json()).then(x => {
-            alert(`The temperature will be ${x.wet}!`);
+}
+const postData = async () => {
+        const request = await fetch(`/`, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                zip: document.all('zip').value
+            })
+        });
+        try {
+            // Transform into JSON
+            const allData = await request.json();
+            console.log(allData)
+            return allData;
+        } catch (error) {
+            console.log("error", error);
+            alert('System Error');
+        }
+
+    }
+    (function () {
+        document.querySelector("#generate").addEventListener("click", function (e) {
+
+            retrieveData().then(allData => {
+                console.log(allData);
+                document.all('feelings').value = '';
+                document.all('zip').value = '';
+                document.getElementById('temp').innerHTML = (Math.round((allData.temp - 273.15) * 9 / 5 + 32)) + ' degrees';
+                document.getElementById('content').innerHTML = allData.feel;
+                document.getElementById("date").innerHTML = new Date(allData.date * 1000).toLocaleString();
+            });
 
         });
-    })
-})();
+        document.querySelector("#post").addEventListener("click", function (e) {
+
+            postData().then(allData => {
+                console.log(allData);
+                document.all('feelings').value = '';
+                document.all('zip').value = '';
+                document.getElementById('temp').innerHTML = (Math.round((allData.temp - 273.15) * 9 / 5 + 32)) + ' degrees';
+                document.getElementById('content').innerHTML = allData.feel;
+                document.getElementById("date").innerHTML = new Date(allData.date * 1000).toLocaleString();
+            });
+
+        });
+    })();
